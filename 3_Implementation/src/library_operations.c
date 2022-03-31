9b0b6db1e612ec99eedd323923dc2902025e70f0
Kousikarajasekar <101034305+Kousikarajasekar@users.noreply.github.com>
2022-03-31 10:42:23 +0530

    Update library_operations.c

--- 3_Implementation/src/library_operations.c
+++ 3_Implementation/src/library_operations.c
@@ -1,1 +1,24 @@
+#include <library_operations.h>
 
+void issueBook(int id, int m.Name)
+{
+    return issueBook with time;
+}
+
+void issueList(int id, int m.Name )
+{
+    return issueList;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
